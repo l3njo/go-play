7d3a5187b418fec2c1e0afcc4c3f6f3e6de214ad
l3njo <plenjo15@gmail.com>
2019-06-04 12:26:20 +0300

    Implement basic card functionality

--- card/card.go
+++ card/card.go
@@ -8,8 +8,9 @@ type card struct {
 	face, suit string
 }
 
-var faces = [...]string{""}
-var suits = [...]string{""}
+// Card face value
+var faces = [...]string{"Ace", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"}
+var suits = [...]string{"Spades", "Hearts", "Clubs", "Diamonds"}
 
 func (cardStruct *card) getCard() string {
 	return cardStruct.face + " of " + cardStruct.suit
