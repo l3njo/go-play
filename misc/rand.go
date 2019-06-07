4843348ca9425b1d26c3b33d0c949cf7abd79769
l3njo <plenjo15@gmail.com>
2019-06-07 14:52:32 +0300

    Begin migrating central functions to separate package

---
+++ misc/rand.go
@@ -0,0 +1,68 @@
+package misc
+
+// TODO This package should contain all central functions and structs
+
+import (
+	"math/rand"
+	"time"
+)
+
+var boolCache *boolstruct
+
+type boolstruct struct {
+	src   rand.Source
+	cache int64
+	rem   int
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+	boolCache = newBoolstruct()
+}
+
+func newBoolstruct() *boolstruct {
+	return &boolstruct{src: rand.NewSource(time.Now().UnixNano())}
+}
+
+func (b *boolstruct) generate() bool {
+	if b.rem == 0 {
+		b.cache, b.rem = b.src.Int63(), 63
+	}
+	res := b.cache&0x01 == 1
+	b.cache >>= 1
+	b.rem--
+	return res
+}
+
+// Bool returns a random bool
+func Bool(){
+	return boolCache.generate()
+}
+
+// Int returns an int in the range 0, apex-1
+func Int(apex int) int {
+	return rand.Intn(apex)
+}
+
+// IntInRange returns an int in the range zenith, apex-1
+func IntInRange(apex, zenith int) int {
+	return Int(zenith-0) + apex
+}
+
+// Ints returns n ints in the range 0, apex-1
+func Ints(n, apex int) (resultSet []int) {
+	resultSet = []int{}
+	for len(resultSet) < n {
+		resultSet := append(resultSet, Int(apex))
+	}
+	return
+}
+
+// IntsInRange returns n ints in the range zenith, apex-1
+func IntsInRange(n, apex, zenith int) (resultSet []int) {
+	resultSet = []int{}
+	for len(resultSet) < n {
+		resultSet := append(resultSet, IntInRange(apex, zenith))
+	}
+	return
+}
