65aa5ccd244e73d91a3eb891b0d5745b807930e6
l3njo <plenjo15@gmail.com>
2019-06-07 16:04:59 +0300

    Migrate dice package dependencies to misc

--- dice/dice.go
+++ dice/dice.go
@@ -2,13 +2,9 @@ package dice
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
-)
 
-func init() {
-	rand.Seed(time.Now().UTC().UnixNano())
-}
+	"github.com/l3njo/play/misc"
+)
 
 // Roll returns random results for num dice of sides faces each
 // Returns an error if num <= 0 || sides <= 2
@@ -24,19 +20,19 @@ func Roll(num, sides int) (rolls []int, err string) {
 		return
 	}
 	for len(rolls) < num {
-		rolls = append(rolls, rand.Intn(sides)+1)
+		rolls = append(rolls, misc.Int(sides)+1)
 	}
 	return
 }
 
 // RollUneven returns results for a number of dice of sides faces
-// Returns 0 and an error for any side <= 2
+// Returns 0 and an error for any side that is <= 2
 func RollUneven(sides ...int) (unevenRolls []int, err string) {
 	unevenRolls = []int{}
 	err = ""
 	for i, v := range sides {
 		if v > 2 {
-			unevenRolls = append(unevenRolls, rand.Intn(v)+1)
+			unevenRolls = append(unevenRolls, misc.Int(v)+1)
 			continue
 		}
 		err = err + fmt.Sprintf("Error on input %v.\n", i)
