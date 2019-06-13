c5bd6d5ea408af15eb9b4aca69a27899c4960685
l3njo <plenjo15@gmail.com>
2019-06-13 19:10:42 +0300

    Implement rps methods

--- misc/vars.go
+++ misc/vars.go
@@ -12,6 +12,18 @@ var Faces = [...]string{"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Ja
 // Suits is a slice of strings describing card suits.
 var Suits = [...]string{"Spades", "Hearts", "Clubs", "Diamonds"}
 
+// Choices is an array of choices for the rock, paper, scissors game.
+var Choices = [...]string{"rock", "paper", "scissors"}
+
+// ChoiceMap is a map of choices for the rock, paper, scissors game.
+// It is in the format:
+// choice: beats
+var ChoiceMap = map[string]string{
+	"rock":     "scissors",
+	"paper":    "rock",
+	"scissors": "paper",
+}
+
 type boolstruct struct {
 	src   rand.Source
 	cache int64
--- rock/rock.go
+++ rock/rock.go
@@ -7,38 +7,65 @@ import (
 	"github.com/l3njo/play/misc"
 )
 
-func main() {
-	var choice string
-	choices := map[int]int{
-		1: 0,
-		0: 2,
-		2: 1,
-	}
-
-	computerMove := misc.Int(3)
-	humanMove := -1
-
-	fmt.Scanf("%s\n", &choice)
-	choice = strings.ToLower(choice)
-
-	if choice == "rock" {
-		humanMove = 0
-	} else if choice == "paper" {
-		humanMove = 1
-	} else if choice == "scissors" {
-		humanMove = 2
-	}
-
-	if humanMove != -1 {
-		switch humanMove {
-		case computerMove:
-			fmt.Println("Tie, Replay!")
-		case choices[computerMove]:
-			fmt.Println("Computer Wins!")
-			break loop
-		default:
-			fmt.Println("You Win!")
-			break loop
-		}
-	}
+// Get returns a random value from {"rock", "paper", "scissors"}
+func Get() string {
+	return misc.Choices[misc.Int(len(misc.Choices))]
 }
+
+
+// Play returns a string stating what each player chose and a bool for whether the user won.
+func Play(userChoice string) (result string) {
+	result = ""
+	_, valid := misc.ChoiceMap[userChoice]
+	if !valid {
+		result = "Error! Invalid choice."
+		return
+	}
+	userChoice = strings.ToLower(userChoice)
+	computerChoice := misc.Choices[misc.Int(len(misc.Choices))]
+	switch userChoice {
+		case computerChoice:
+			result = fmt.Sprintf("You drew! You both chose %v.", computerChoice)
+		case misc.ChoiceMap[computerChoice]:
+			result = fmt.Sprintf("You lost! You chose %v and the computer chose %v.", userChoice, computerChoice)
+		default:
+			result = fmt.Sprintf("You won! You chose %v and the computer chose %v.", userChoice, computerChoice)
+	}
+	return
+}
+
+// func main() {
+// 	var choice string
+// 	choices := map[int]int{
+// 		1: 0,
+// 		0: 2,
+// 		2: 1,
+// 	}
+
+// 	computerMove := misc.Int(3)
+// 	humanMove := -1
+
+// 	fmt.Scanf("%s\n", &choice)
+// 	choice = strings.ToLower(choice)
+
+// 	if choice == "rock" {
+// 		humanMove = 0
+// 	} else if choice == "paper" {
+// 		humanMove = 1
+// 	} else if choice == "scissors" {
+// 		humanMove = 2
+// 	}
+
+// 	if humanMove != -1 {
+// 		switch humanMove {
+// 		case computerMove:
+// 			fmt.Println("Tie, Replay!")
+// 		case choices[computerMove]:
+// 			fmt.Println("Computer Wins!")
+// 			break loop
+// 		default:
+// 			fmt.Println("You Win!")
+// 			break loop
+// 		}
+// 	}
+// }
